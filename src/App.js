5644bfaf47f0181c18dad046ed28f06f89aac679
RichDobrz <rjdobrz@gmail.com>
2019-11-01 05:37:35 -0400

    node updates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import Home from "./components/Home/Home"
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import './App.css';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react"
+
+
+const Home = () => {
+  return(
+    <div className="main">
+      <p>Home is working</p>
+    </div>
+  )
+}
+
+export default Home
