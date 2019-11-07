ad7de029adc70984e9eacf5a50b0c545185986ab
RichDobrz <rjdobrz@gmail.com>
2019-11-07 14:06:41 -0500

    object display for home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,18 +7,24 @@ const Home = () => {
   return(
     <div className="main">
       <div className="intro">
-        <h1>Richard Dobrzeniecki</h1>
-        <h2>Web Developer</h2>
-        <div className="socials">
-          <a href="http://github.com/RichDobrz"><img src={github} alt=""/></a>
-          <a href="https://www.linkedin.com/in/richard-dobrz/"><img src={linkedin} alt=""/></a>
-        </div>
-        <div className="enter">
-          ENTER
-        </div>
+        <h1> const richDobrzeniecki = <span className="curlys">&#123;</span>
+          <p className="firstIndent">navBar: <span className="curlys">&#123;</span></p>
+          <p className="secondIndent">id: Web Developer,</p>
+          <p className="secondIndent">about: Link,</p>
+          <p className="secondIndent">portfolio: Projects,</p>
+          <p className="secondIndent">contact: Link</p>
+          <div className="curlys firstIndent">&#125;</div>
+          <div className="curlys">&#125;</div>
+        </h1>
       </div>
     </div>
   )
 }
+
+{/* <h2>Web Developer</h2>
+<div className="socials">
+  <a href="http://github.com/RichDobrz"><img src={github} alt=""/></a>
+  <a href="https://www.linkedin.com/in/richard-dobrz/"><img src={linkedin} alt=""/></a>
+</div> */}
 
 export default Home
